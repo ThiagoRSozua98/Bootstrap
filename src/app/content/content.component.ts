82ee82799198b24f2e1f0c9e0cd29d02bad6f85e
Thiago <thiago.reiissouza@gmail.com>
2019-09-20 22:27:09 -0300

    Implementação de Animação

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import Typed from 'typed.js';
+
+
 @Component({
   selector: 'has-content',
   templateUrl: './content.component.html',
@@ -10,6 +13,20 @@ export class ContentComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const options = {
+      stringsElement: '#typed-strings',
+      strings: ['NuBank', 'Peça a portabilidade e venha ser NuBank hoje mesmo', 'Inovador, digital e seguro'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 200,
+      smartBackspace: true,
+      fadeOut: true,
+      showCursor: false,
+      startDelay: 1000,
+      loop: true
+    };
+	
+	const typed = new Typed('.typing-element', options);
   }
 
 }
